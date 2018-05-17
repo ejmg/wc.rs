8fd6bdd0a37f98542c54fe5094e30cb058acf1ac
ejmg <elias.jm.garcia@gmail.com>
2018-05-17 18:08:23 -0500

    refactoring into proper bin format as expected

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use std::io::prelude::*;
 use std::fs::File;
 
 
+struct Args {
+    flag: String,
+    filename: String,
+}
+
 fn wc() -> u64 {
     unimplemented!()
 }
@@ -11,18 +16,31 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let flag: &String = &args[1];
-    let file: &String = &args[2];
+    // TODO: unwrap this arg
+    let args = Args::new(&args).unwrap_or_else(|err| {
+        println!("shit is fucked, args not parsed!", );
+    });
 
-    println!("flag: {:?}", flag);
-    println!("file: {:?}", file);
+    println!("flag: {:?}", args.flag);
+    println!("file: {:?}", args.filename);
 
-    let mut f: File = File::open(file).expect("file not found");
-
+    let mut f: File = File::open(args.filename).expect("file not found");
     let mut contents: String = String::new();
 
     f.read_to_string(&mut contents).expect("file contents whack");
 
-    println!("file contents:\n {}", contents);
+    println!("file contents:\n{}", contents);
     
+}
+
+impl Args {
+    fn new(args: &[String]) -> Result<Args, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough args, fool.");
+        }
+        let flag: String = args[1].clone();
+        let filename: String = args[2].clone();
+
+        Ok(Args{ flag, filename })
+    }
 }
